ef8a453eaffc7689049b3a8fd6370bbb6a068318
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-11 18:25:42 +0800

    Delete build-garuda-master.sh

--- build-garuda-master.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-svn checkout http://www.opencloudengine.org/svn/garuda/trunk/garuda-master
-cd garuda-master/
-mvn install package
-
-rm -rf lib
-cp target/garuda-master-1.0.0-SNAPSHOT.jar .
-cp -R target/lib .
