608704d2adb424dedbf8c8189e5a222abcc3d359
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-11 18:28:56 +0800

    Delete download-garuda-runtime.sh

--- download-garuda-runtime.sh
+++
@@ -1,7 +0,0 @@
-wget https://www.dropbox.com/s/kmv524deo3lr9d9/felix-framework-4.4.0.tar.gz
-wget https://www.dropbox.com/s/7vvdrq9ou3o39sw/ArvueAppServerController.tar
-gunzip felix-framework-4.4.0.tar.gz
-
-wget https://www.dropbox.com/s/cuprhsdgjomdgbq/felix
-wget https://www.dropbox.com/s/kpzx25rp88ne64l/appServerController
-wget https://www.dropbox.com/s/0macmkd23k3hap7/supervisord.conf
